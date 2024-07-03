a6865cb0bb553d4868c4b49feb3a8b3f501e3da3
LBang98 <dlqudgus98@gmail.com>
2024-07-03 10:12:22 +0900

    test

--- springwork/BootJPAEx1/src/test/java/com/example/bootjpaex1/BootJpaEx1ApplicationTests.java
+++ springwork/BootJPAEx1/src/test/java/com/example/bootjpaex1/BootJpaEx1ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.soccer;
+package com.example.bootjpaex1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SoccerApplicationTests {
+class BootJpaEx1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
