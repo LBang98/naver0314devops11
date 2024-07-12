cf87be2a88df6530bdad8bebe1bc322ff3dc5556
B H <bh@Bui-MacBookAir.local>
2024-07-12 16:29:14 +0900

    7월12일

---
+++ reactwork/react-day0711/src/components/FiveApp.js
@@ -0,0 +1,12 @@
+import { Alert } from '@mui/material';
+import React from 'react';
+
+const FiveApp = () => {
+    return (
+        <div>
+            <Alert>FiveApp-Axios db데이타가져오기</Alert>
+        </div>
+    );
+};
+
+export default FiveApp;
